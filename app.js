023ea17ed07ff2defeefbf05238675927e29ea7e
Thor-Dahl <thor.ariasdahl@gmail.com>
2026-04-27 07:28:59 +0800

    implement frontend

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const API_KEY = 'fbd75a4a77ee167ea4d5ca7db8646232';
-const city = 'Manila';
-
 async function getWeather() {
+    const city = $('#city-input').val().trim();
+
     const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
     const data = await res.json();
 
     console.log(data.name);
     console.log(data.main.temp);
     console.log(data.weather[0].description);
+
+    const weatherCard = `
+        <div class="weather-card">
+            <p id="city-name">${data.name}</p>
+            <p id="temperature">${data.main.temp}</p>
+            <p id="description">${data.weather[0].description}</p>
+            <p id="humidity">${data.main.humidity}%</p>
+        </div>
+    `;
+
+    $('.weather-card-container').append(weatherCard);
 }
 
-getWeather();
+$('#get-weather-btn').on('click', function() {
+    getWeather();
+});

e392a134f6f41410486e6ae972958b42998603f4
Thor-Dahl <thor.ariasdahl@gmail.com>
2026-04-29 07:00:05 +0800

    implement dynamic weather icon

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ async function getWeather() {
     const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
     const data = await res.json();
 
-    console.log(data.name);
-    console.log(data.main.temp);
-    console.log(data.weather[0].description);
+    const weatherIcon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
     const weatherCard = `
         <div class="weather-card">
-            <p id="city-name">${data.name}</p>
-            <p id="temperature">${data.main.temp}</p>
-            <p id="description">${data.weather[0].description}</p>
-            <p id="humidity">${data.main.humidity}%</p>
+            <div class="temperature">
+                <img src="${weatherIcon}" id="weather-icon"/>
+                <p id="temperature-value">${Math.round(data.main.temp)}°C</p>
+            </div>
+            <div class="city">
+                <p id="city-name">${data.name}</p>
+                <p id="country-name">feels like ${Math.round(data.main.feels_like)}°C</p>
+            </div>
+            <div class="other-info">
+                <p id="description">${data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1)}</p>
+                <p id="humidity">${data.main.humidity}%</p>
+                <p id="humidity">${data.wind.speed} km/h</p>
+            </div>
         </div>
     `;
 
@@ -25,11 +32,11 @@ $('#get-weather-btn').on('click', function() {
 });
 
 /*
- 1.) fix alignment of values
- 2.) implement a header identifying column values
- 3.) implement feels like
- 4.) implement quick emoji based on WMO weathercode
- 5.) implement wind speed
+ ~ 1.) fix alignment of values
+ ~ 2.) implement a header identifying column values
+ ~ 3.) implement feels like
+ ~ 4.) implement quick emoji based on WMO weathercode
+ ~ 5.) implement wind speed
  6.) implement remove card
  7.) handle wrong searches
  8.) handle all four UI states

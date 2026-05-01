c4eee65d176be06799128e0f2a6f08c62cc6de24
Thor-Dahl <thor.ariasdahl@gmail.com>
2026-05-01 17:12:59 +0800

    add four UI states and validation check

--- app.js
+++ app.js
@@ -10,45 +10,70 @@ async function getWeather() {
     const city = $('#city-input').val().trim();
 
     setState('loading');
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-    const data = await res.json();
-    setState('success');
-    const weatherIcon = getWeatherIconURL(data.weather[0].icon);
+    try {
+        // get weather data
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+        const data = await res.json();
 
-    const weatherCard = `
-        <div class="weather-card">
-            <button class="remove-btn">✕</button>
-            <div class="temperature">
-                <img src="${weatherIcon}" id="weather-icon"/>
-                <p id="temperature-value">${Math.round(data.main.temp)}°C</p>
-            </div>
-            <div class="city">
-                <p id="city-name">${data.name}</p>
-                <p id="country-name">feels like ${Math.round(data.main.feels_like)}°C</p>
-            </div>
-            <div class="other-info">
-                <p id="description">${capitalizeFL(data.weather[0].description)}</p>
-                <p id="humidity">${data.main.humidity}%</p>
-                <p id="humidity">${data.wind.speed} km/h</p>
-            </div>
-        </div>
-    `;
+        // handle wrong search
+        if (res.ok === false) {
+            setState('error');
+            return;
+        }
+        setState('success');
 
-    //remove card
-    const $card = $(weatherCard);
-    $card.find('.remove-btn').on('click', function() {
-        $card.remove()
-    });
+        //build weather card
+        const weatherIcon = getWeatherIconURL(data.weather[0].icon);
+        const weatherCard = `
+            <div class="weather-card">
+                <button class="remove-btn">✕</button>
+                <div class="temperature">
+                    <img src="${weatherIcon}" id="weather-icon"/>
+                    <p id="temperature-value">${Math.round(data.main.temp)}°C</p>
+                </div>
+                <div class="city">
+                    <p id="city-name">${data.name}</p>
+                    <p id="country-name">feels like ${Math.round(data.main.feels_like)}°C</p>
+                </div>
+                <div class="other-info">
+                    <p id="description">${capitalizeFL(data.weather[0].description)}</p>
+                    <p id="humidity">${data.main.humidity}%</p>
+                    <p id="humidity">${data.wind.speed} km/h</p>
+                </div>
+            </div>
+        `;
+        //remove card
+        const $card = $(weatherCard);
+        $card.find('.remove-btn').on('click', function() {
+            $card.remove()
+        });
 
-    $('.weather-card-container').append($card);
+        $('.weather-card-container').append($card);
+    } catch {
+        setState('error');;
+    }
+    
 }
 
 async function getSuggestions() {
     const city = $('#city-input').val().trim();
+
+    if (!city) {
+        $('.suggestions-container').empty().hide();
+        return;
+    }
+
     const res = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`)
     const data = await res.json();
 
     $('.suggestions-container').empty();
+
+    if (data.length === 0) {
+        $('.suggestions-container').hide();
+        return;
+    }
+
+    $('.suggestions-container').css('display', 'flex');
 
     data.forEach(function(sug) {
         const city = sug.name;
@@ -65,7 +90,7 @@ async function getSuggestions() {
         $item.on('click', function() {
             $('#city-input').val(city);
             getWeather();
-            $('.suggestions-container').empty();
+            $('.suggestions-container').empty().hide();
             $('#city-input').val('');
         })
 
@@ -79,7 +104,7 @@ function setState(state) {
         case 'loading': 
             const loadingItem = `
                 <div class="loading weather-card" id="loading-card">
-                <p class="loading-icon">...</p>
+                    <p class="loading-icon">...</p>
                 </div>
             `;
             $('#loading-card').remove();
@@ -88,7 +113,15 @@ function setState(state) {
         case 'success': 
             $('#loading-card').remove();
             break;
-        case 'error': break;
+        case 'error': 
+            const errorItem = `
+                <div class="error weather-card" id="error-card">
+                    <p class="error-text">Something went wrong...</p>
+                </div>
+            `;
+            $('#loading-card').remove();
+            $('.weather-card-container').append(errorItem)
+            break;
     }
 }
 
